5b12c5d5f03bdf4cd05bfc43ce1960f961572d28
Caden Silver <cadensilver@MacBook-Pro.ultrahub>
2026-04-20 18:25:41 +0100

    Add manual sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://appealairportpcn.co.uk',
   compressHTML: true,
+  integrations: [sitemap()],
 });
