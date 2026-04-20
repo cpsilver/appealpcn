a252c81eb759e59a747fade69da1318c581e96f8
Caden Silver <cadensilver@MacBook-Pro.ultrahub>
2026-04-20 23:31:38 +0100

    new appeal text, tab card redesign, blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
-
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://appealairportpcn.co.uk',
-  compressHTML: true,
+  site: 'https://appealpcn.co.uk',
   integrations: [sitemap()],
+  compressHTML: true,
 });
